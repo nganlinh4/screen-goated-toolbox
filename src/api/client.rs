d4428d705d031d797e46560004c48111ac01f388
nganlinh4 <nganlinh4@gmail.com>
2025-12-01 14:44:23 +0900

    feat: Introduce global ureq client agent with 120s read/write timeouts.

--- src/api/client.rs
+++ src/api/client.rs
@@ -2,7 +2,7 @@ use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref UREQ_AGENT: ureq::Agent = ureq::AgentBuilder::new()
-        .timeout_read(std::time::Duration::from_secs(30))
-        .timeout_write(std::time::Duration::from_secs(30))
+        .timeout_read(std::time::Duration::from_secs(120))
+        .timeout_write(std::time::Duration::from_secs(120))
         .build();
 }
